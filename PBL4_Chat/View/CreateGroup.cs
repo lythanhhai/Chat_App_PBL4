c072172258062b213161ae48c90e2fb85bf7ce1d
lythanhhai <you@example.com>
2021-10-15 20:56:42 +0700

    them chuc nang chat nhom

---
+++ PBL4_Chat/View/CreateGroup.cs
@@ -0,0 +1,193 @@
+﻿using Bunifu.UI.WinForms;
+using PBL4_Chat.BLL;
+using PBL4_Chat.DTO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PBL3_DATVEXE.View
+{
+    public partial class datVe : Form
+    {
+        // delegate lấy số vé và tổng giá
+        public delegate void getVeAndTongGia(int soVe,double tongGia);
+
+        public getVeAndTongGia d1 { get; set; }
+
+        // lấy id_route and id_vehicle từ confirm sang datVe
+        public delegate string getIdRouteAndVehicle();
+
+        public getIdRouteAndVehicle getRoute { get; set; }
+        public getIdRouteAndVehicle getVehicle { get; set; }
+
+        // lấy tên ghế từ form confirm sang form datVe.
+        public delegate List<string> getNameGhe();
+
+        public getNameGhe getGhe { get; set; }
+        public int soVe { get; set; }
+        public double tongGia { get; set; }
+
+        public string id_detRoute { get; set; }
+
+        public string id_vehicle { get; set; }
+        public datVe(int soVe,double tongGia)
+        {
+            InitializeComponent();
+            // giá và số vé
+            this.soVe = soVe;
+            this.tongGia = tongGia;
+            lbTen.ForeColor = Color.FromArgb(78, 184, 206);
+            bunifuPanel1.BackgroundColor = Color.FromArgb(78, 184, 206);
+            txtName.ForeColor = Color.FromArgb(78, 184, 206);
+        }
+
+        private void But_xacnhan_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtName_Click(object sender, EventArgs e)
+        {
+        //    txtName.Clear();
+            lbTen.ForeColor = Color.FromArgb(78, 184, 206);
+            bunifuPanel1.BackgroundColor = Color.FromArgb(78, 184, 206);
+            txtName.ForeColor = Color.FromArgb(78, 184, 206);
+
+            lbSoDT.ForeColor = Color.White;
+            bunifuPanel2.BackgroundColor = Color.White;
+            txtPhone.ForeColor = Color.White;
+
+            lbEmail.ForeColor = Color.White;
+            bunifuPanel3.BackgroundColor = Color.White;
+            txtEmail.ForeColor = Color.White;
+
+            lbNote.ForeColor = Color.White;
+            bunifuPanel4.BackgroundColor = Color.White;
+            txtNote.ForeColor = Color.White;
+        }
+
+        private void txtPhone_Click(object sender, EventArgs e)
+        {
+          //  txtPhone.Clear();
+            lbSoDT.ForeColor = Color.FromArgb(78, 184, 206);
+            bunifuPanel2.BackgroundColor = Color.FromArgb(78, 184, 206);
+            txtPhone.ForeColor = Color.FromArgb(78, 184, 206);
+
+            lbTen.ForeColor = Color.White;
+            bunifuPanel1.BackgroundColor = Color.White;
+            txtName.ForeColor = Color.White;
+
+            lbEmail.ForeColor = Color.White;
+            bunifuPanel3.BackgroundColor = Color.White;
+            txtEmail.ForeColor = Color.White;
+
+            lbNote.ForeColor = Color.White;
+            bunifuPanel4.BackgroundColor = Color.White;
+            txtNote.ForeColor = Color.White;
+        }
+
+        private void txtEmail_Click(object sender, EventArgs e)
+        {
+           // txtEmail.Clear();
+            lbEmail.ForeColor = Color.FromArgb(78, 184, 206);
+            bunifuPanel3.BackgroundColor = Color.FromArgb(78, 184, 206);
+            txtEmail.ForeColor = Color.FromArgb(78, 184, 206);
+
+            lbSoDT.ForeColor = Color.White;
+            bunifuPanel2.BackgroundColor = Color.White;
+            txtPhone.ForeColor = Color.White;
+
+            lbTen.ForeColor = Color.White;
+            bunifuPanel1.BackgroundColor = Color.White;
+            txtName.ForeColor = Color.White;
+
+            lbNote.ForeColor = Color.White;
+            bunifuPanel4.BackgroundColor = Color.White;
+            txtNote.ForeColor = Color.White;
+        }
+
+        private void txtNote_Click(object sender, EventArgs e)
+        {
+         //   txtNote.Clear();
+            lbNote.ForeColor = Color.FromArgb(78, 184, 206);
+            bunifuPanel4.BackgroundColor = Color.FromArgb(78, 184, 206);
+            txtNote.ForeColor = Color.FromArgb(78, 184, 206);
+            
+
+            lbSoDT.ForeColor = Color.White;
+            bunifuPanel2.BackgroundColor = Color.White;
+            txtPhone.ForeColor = Color.White;
+
+            lbEmail.ForeColor = Color.White;
+            bunifuPanel3.BackgroundColor = Color.White;
+            txtEmail.ForeColor = Color.White;
+
+            lbTen.ForeColor = Color.White;
+            bunifuPanel1.BackgroundColor = Color.White;
+            txtName.ForeColor = Color.White;
+        }
+
+        private void But_xemthongtin_Click(object sender, EventArgs e)
+        {
+            // showNameSeat();
+            panelMain.Hide();
+          //  OpenChildForm(new infoTicket(this.id_detRoute,this.id_vehicle,this.tongGia));
+        }
+        private Form currentChildForm;
+        private void OpenChildForm(Form childForm)
+        {
+            //open only form
+            if (currentChildForm != null)
+            {
+                currentChildForm.Close();
+            }
+            currentChildForm = childForm;
+            //End
+            childForm.TopLevel = false;
+            childForm.FormBorderStyle = FormBorderStyle.None;
+            childForm.Dock = DockStyle.Fill;
+            panelMain.Controls.Add(childForm);
+            //panelMain.Tag = childForm;
+            childForm.BringToFront();
+            childForm.Show();
+            panelMain.Text = childForm.Text;
+        }
+
+        private void but_troveConfirm_Click(object sender, EventArgs e)
+        {
+            Form frm = Application.OpenForms["DetailSchedule"];
+            frm.Show();
+            this.Hide();
+        }
+
+        private void datVe_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Form frm = Application.OpenForms["DetailSchedule"];
+            frm.Show();
+            this.Hide();
+        }
+        //public void showNameSeat()
+        //{
+        //    List<string> listName = getGhe();
+        //    //List<seeTicket> list = new List<seeTicket>();
+        //    seeTicket[] list = new seeTicket[listName.Count];
+        //    int count1 = 0;
+        //    foreach(seeTicket i in list)
+        //    {
+
+        //        flowLayoutPanel1.Controls.Add(new seeTicket
+        //        {
+        //             NameGhe = listName[count1],
+        //             Gia = (this.tongGia / this.soVe).ToString(),
+        //        });
+        //        count1++;
+        //    }    
+        //}
+    }
+}
---
+++ PBL4_Chat/View/group_info.Designer.cs
@@ -0,0 +1,119 @@
+﻿
+namespace PBL4_Chat.View
+{
+    partial class group_info
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(group_info));
+            this.gunaGradientPanel1 = new Guna.UI.WinForms.GunaGradientPanel();
+            this.lbDes = new Guna.UI.WinForms.GunaLabel();
+            this.lbNameGroup = new Guna.UI.WinForms.GunaLabel();
+            this.gunaCirclePictureBox1 = new Guna.UI.WinForms.GunaCirclePictureBox();
+            this.gunaGradientPanel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaCirclePictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gunaGradientPanel1
+            // 
+            this.gunaGradientPanel1.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("gunaGradientPanel1.BackgroundImage")));
+            this.gunaGradientPanel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.gunaGradientPanel1.Controls.Add(this.lbDes);
+            this.gunaGradientPanel1.Controls.Add(this.lbNameGroup);
+            this.gunaGradientPanel1.Controls.Add(this.gunaCirclePictureBox1);
+            this.gunaGradientPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gunaGradientPanel1.GradientColor1 = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(36)))), ((int)(((byte)(206)))));
+            this.gunaGradientPanel1.GradientColor2 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(48)))), ((int)(((byte)(90)))));
+            this.gunaGradientPanel1.GradientColor3 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(48)))), ((int)(((byte)(90)))));
+            this.gunaGradientPanel1.GradientColor4 = System.Drawing.Color.FromArgb(((int)(((byte)(250)))), ((int)(((byte)(48)))), ((int)(((byte)(90)))));
+            this.gunaGradientPanel1.Location = new System.Drawing.Point(0, 0);
+            this.gunaGradientPanel1.Name = "gunaGradientPanel1";
+            this.gunaGradientPanel1.Size = new System.Drawing.Size(384, 95);
+            this.gunaGradientPanel1.TabIndex = 0;
+            this.gunaGradientPanel1.Text = "gunaGradientPanel1";
+            // 
+            // lbDes
+            // 
+            this.lbDes.AutoSize = true;
+            this.lbDes.BackColor = System.Drawing.Color.Transparent;
+            this.lbDes.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbDes.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lbDes.ForeColor = System.Drawing.Color.White;
+            this.lbDes.Location = new System.Drawing.Point(80, 54);
+            this.lbDes.Name = "lbDes";
+            this.lbDes.Size = new System.Drawing.Size(165, 25);
+            this.lbDes.TabIndex = 5;
+            this.lbDes.Text = "hello, how are you?";
+            // 
+            // lbNameGroup
+            // 
+            this.lbNameGroup.AutoSize = true;
+            this.lbNameGroup.BackColor = System.Drawing.Color.Transparent;
+            this.lbNameGroup.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.lbNameGroup.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lbNameGroup.ForeColor = System.Drawing.Color.White;
+            this.lbNameGroup.Location = new System.Drawing.Point(80, 16);
+            this.lbNameGroup.Name = "lbNameGroup";
+            this.lbNameGroup.Size = new System.Drawing.Size(59, 25);
+            this.lbNameGroup.TabIndex = 4;
+            this.lbNameGroup.Text = "Hai Ly";
+            // 
+            // gunaCirclePictureBox1
+            // 
+            this.gunaCirclePictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.gunaCirclePictureBox1.BaseColor = System.Drawing.Color.White;
+            this.gunaCirclePictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("gunaCirclePictureBox1.Image")));
+            this.gunaCirclePictureBox1.Location = new System.Drawing.Point(15, 16);
+            this.gunaCirclePictureBox1.Name = "gunaCirclePictureBox1";
+            this.gunaCirclePictureBox1.Size = new System.Drawing.Size(59, 63);
+            this.gunaCirclePictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.gunaCirclePictureBox1.TabIndex = 3;
+            this.gunaCirclePictureBox1.TabStop = false;
+            this.gunaCirclePictureBox1.UseTransfarantBackground = false;
+            // 
+            // group_info
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.gunaGradientPanel1);
+            this.Name = "group_info";
+            this.Size = new System.Drawing.Size(384, 95);
+            this.gunaGradientPanel1.ResumeLayout(false);
+            this.gunaGradientPanel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaCirclePictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI.WinForms.GunaGradientPanel gunaGradientPanel1;
+        private Guna.UI.WinForms.GunaLabel lbDes;
+        private Guna.UI.WinForms.GunaLabel lbNameGroup;
+        private Guna.UI.WinForms.GunaCirclePictureBox gunaCirclePictureBox1;
+    }
+}
